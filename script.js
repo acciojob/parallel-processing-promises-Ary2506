4097693df7691279fd2bc0ef272313f9b11e862b
Ary2506 <ap1895284@gmail.com>
2026-06-09 16:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 const output = document.getElementById("output");
 const loading = document.getElementById("loading");
 const error = document.getElementById("error");
+const button = document.getElementById("download-images-button");
+
+const imageUrls = [
+  "https://picsum.photos/id/237/200/300",
+  "https://picsum.photos/id/238/200/300",
+  "https://picsum.photos/id/239/200/300"
+];
 
 function downloadImage(url) {
   return new Promise((resolve, reject) => {
     const img = document.createElement("img");
 
     img.onload = () => resolve(img);
+
     img.onerror = () => reject(`Failed to load image: ${url}`);
 
     img.src = url;
   });
 }
 
-async function downloadImages(imageUrls) {
+function downloadImages(urls) {
   loading.innerHTML = '<div class="spinner"></div>';
-  error.textContent = '';
-  output.innerHTML = '';
+  error.textContent = "";
+  output.innerHTML = "";
 
-  try {
-    const images = await Promise.all(
-      imageUrls.map(url => downloadImage(url))
-    );
+  Promise.all(urls.map(downloadImage))
+    .then((images) => {
+      loading.innerHTML = "";
 
-    loading.innerHTML = '';
-
-    images.forEach(img => {
-      output.appendChild(img);
+      images.forEach((img) => {
+        output.appendChild(img);
+      });
+    })
+    .catch((err) => {
+      loading.innerHTML = "";
+      error.textContent = err;
     });
-  } catch (err) {
-    loading.innerHTML = '';
-    error.textContent = err;
-  }
 }
+
+button.addEventListener("click", () => {
+  downloadImages(imageUrls);
+});
